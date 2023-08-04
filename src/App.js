9e8fe1a571af66a34f1df0eb13efbde37768f353
Krishnakumar V <krishkumarv2001@gmail.com>
2023-08-04 19:18:45 +0530

    map ui implemented and places marked with mock data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map, Marker } from "pigeon-maps";
+
+const stations = [
+  {
+    id: 1,
+    latitute: 13.9,
+    longitude: 67.8,
+    battery_count: 3,
+    details: {
+      name: "HSR",
+      batteries: 3,
+      contact: "87363733",
+    },
+  },
+  {
+    id: 2,
+    latitute: 14.9,
+    longitude: 66.8,
+    battery_count: 0,
+    details: {
+      name: "HSR",
+      batteries: 3,
+      contact: "87363733",
+    },
+  },
+  {
+    id: 3,
+    latitute: 15.9,
+    longitude: 65.8,
+    battery_count: 1,
+    details: {
+      name: "HSR",
+      batteries: 3,
+      contact: "87363733",
+    },
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Map height={1000} defaultCenter={[14.9, 66.8]} defaultZoom={10}>
+        {stations.map((coords) => (
+          <Marker
+            width={50}
+            anchor={[coords.latitute, coords.longitude]}
+            onClick={() => {
+              alert(coords.latitute + " " + coords.longitude);
+            }}
+          />
+        ))}
+      </Map>
     </div>
   );
 }
