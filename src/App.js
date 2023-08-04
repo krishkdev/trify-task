fc554066098bfaa08880b1435d538d75aa0b9bcc
Krishnakumar V <krishkumarv2001@gmail.com>
2023-08-04 21:40:37 +0530

    optimised code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { Map, Marker, Overlay } from "pigeon-maps";
+import { Map, Marker } from "pigeon-maps";
 import Modal from "react-modal";
 import "./App.css";
+import { stations } from "./data";
+
 const customStyles = {
   content: {
     top: "50%",
@@ -12,64 +14,6 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-
-const stations = [
-  {
-    id: 1,
-    latitute: 13.06801209460255,
-    longitude: 80.25190234073149,
-
-    details: {
-      name: "EVS-1, Nungambakkam",
-      batteries: 2,
-      contact: "7813637331",
-    },
-  },
-  {
-    id: 2,
-    latitute: 13.045609615502588,
-    longitude: 80.24779077921903,
-
-    details: {
-      name: "EVS-2, Teynampet",
-      batteries: 6,
-      contact: "87873363733",
-    },
-  },
-  {
-    id: 3,
-    latitute: 13.041493724404843,
-    longitude: 80.232683837241,
-
-    details: {
-      name: "EVS-3, TNagar",
-      batteries: 0,
-      contact: "9187363733",
-    },
-  },
-  {
-    id: 4,
-    latitute: 13.018904876914265,
-    longitude: 80.27002887517634,
-
-    details: {
-      name: "EVS-4, RA puram",
-      batteries: 0,
-      contact: "9187363733",
-    },
-  },
-  {
-    id: 5,
-    latitute: 13.025529252365498,
-    longitude: 80.22965843136527,
-
-    details: {
-      name: "EVS-5, Saidapet",
-      batteries: 0,
-      contact: "9187363733",
-    },
-  },
-];
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -90,28 +34,25 @@ function App() {
 
   function PlaceList() {
     return (
-      <>
-        <ul>
-          <h1>About place</h1>
-          <h3>
-            Place name:{" "}
-            <span style={{ fontWeight: 400 }}>{modalData.name}</span>
-          </h3>
-          <h3>
-            Batteries left:{" "}
-            <span style={{ fontWeight: 400 }}>{modalData.batteries}</span>
-          </h3>
-          <h3>
-            Contact:{" "}
-            <span style={{ fontWeight: 400 }}>{modalData.contact}</span>
-          </h3>
-        </ul>
-      </>
+      <ul>
+        <h1>About place</h1>
+        <h3>
+          Place name: <span style={{ fontWeight: 400 }}>{modalData.name}</span>
+        </h3>
+        <h3>
+          Batteries left:{" "}
+          <span style={{ fontWeight: 400 }}>{modalData.batteries}</span>
+        </h3>
+        <h3>
+          Contact: <span style={{ fontWeight: 400 }}>{modalData.contact}</span>
+        </h3>
+      </ul>
     );
   }
 
   return (
     <div>
+      <h1 className="title">EV Charging stations</h1>
       <Map height={800} defaultCenter={[13.04, 80.2]} defaultZoom={12}>
         {hideEmptyEVS
           ? stations
---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const stations = [
+  {
+    id: 1,
+    latitute: 13.06801209460255,
+    longitude: 80.25190234073149,
+
+    details: {
+      name: "EVS-1, Nungambakkam",
+      batteries: 2,
+      contact: "7813637331",
+    },
+  },
+  {
+    id: 2,
+    latitute: 13.045609615502588,
+    longitude: 80.24779077921903,
+
+    details: {
+      name: "EVS-2, Teynampet",
+      batteries: 6,
+      contact: "87873363733",
+    },
+  },
+  {
+    id: 3,
+    latitute: 13.041493724404843,
+    longitude: 80.232683837241,
+
+    details: {
+      name: "EVS-3, TNagar",
+      batteries: 0,
+      contact: "9187363733",
+    },
+  },
+  {
+    id: 4,
+    latitute: 13.018904876914265,
+    longitude: 80.27002887517634,
+
+    details: {
+      name: "EVS-4, RA puram",
+      batteries: 0,
+      contact: "9187363733",
+    },
+  },
+  {
+    id: 5,
+    latitute: 13.025529252365498,
+    longitude: 80.22965843136527,
+
+    details: {
+      name: "EVS-5, Saidapet",
+      batteries: 0,
+      contact: "9187363733",
+    },
+  },
+];
